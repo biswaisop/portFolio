1dd0c67f868a5a10ef551b454eaa85d513674959
Biswadip mandal <95963808+biswaisop@users.noreply.github.com>
2024-09-22 17:08:09 +0530

    overlays

---
+++ app/Componrnts/menuOverlay/Menuoverlay.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+import Navlink from "../Navlink/Navlink";
+
+const Menuoverlay = () => {
+  const links = [
+    {
+      title: "About",
+      path: "#about",
+    },
+
+    {
+      title: "Projects",
+      path: "#projects",
+    },
+
+    {
+      title: "Contact",
+      path: "#contact",
+    },
+  ];
+  return (
+    <ul className="flex flex-col py-4 items-center">
+      {links.map((link, inedx) => (
+        <li>
+          <Navlink href={link.path} title={link.title} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Menuoverlay;
