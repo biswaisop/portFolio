fc349e0dacbcc4a7fa1b5ce27f97037dd704501b
Biswadip mandal <95963808+biswaisop@users.noreply.github.com>
2024-09-23 22:45:08 +0530

    aboutupdate

--- app/Componrnts/about/AboutSection.js
+++ app/Componrnts/about/AboutSection.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import { Image } from "next/image";
+import aboutImage from "./images/used.png";
 
 const AboutSection = () => {
   return (
     <section className="text-white ">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-
+        <Image src={aboutImage} width={300} height={300} />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutSection
+export default AboutSection;
