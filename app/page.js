17d3de07c707dca69c024ac16278ce72ce26ea84
Biswadip mandal <95963808+biswaisop@users.noreply.github.com>
2024-09-23 22:21:21 +0530

    aboutfix

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Herosection from "@/app/Componrnts/Herosection/Herosection";
 import Navbar from "./Componrnts/navbar/Navbar";
+import AboutSection from "./Componrnts/about/AboutSection";
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen bg-[#121212] ">
