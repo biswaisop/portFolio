2a9e5bc75ecdfe13e7f0306c2d0fd1a838120bf4
Biswadip mandal <95963808+biswaisop@users.noreply.github.com>
2024-09-24 19:28:12 +0530

    aboutdesc

--- app/Componrnts/about/AboutSection.js
+++ app/Componrnts/about/AboutSection.js
@@ -20,16 +20,22 @@ const AboutSection = () => {
         <div className="text-center md:text-left ml-4 lg:ml-8">
           <h1 className="font-extrabold text-4xl lg:text-6xl mb-4">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500">
-              About {""}
+              About {""} 
             </span>
             Me
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Hello! I'm Biswadip Mandal, a first-semester Computer Science
-            Engineering student at RVITM. I'm passionate about web development
-            and currently honing my skills with the MERN stack. I've already
-            built several projects that showcase my ability to create responsive
-            and dynamic web applications.
+            I&apos;m passionate about web development and currently honing my skills
+            with the MERN stack. I&apos;ve already built several projects that
+            showcase my ability to create responsive and dynamic web
+            applications.
+          </p>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            In addition to web development, I have a solid foundation in Python,
+            demonstrated through various projects that showcase my
+            problem-solving skills. I&apos;m also familiar with SQL, enabling me to
+            manage and interact with databases effectively. I&apos;m excited to
+            continue learning and take on new challenges in my tech journey.
           </p>
 
           <ul>
@@ -47,17 +53,22 @@ const AboutSection = () => {
                 Projects
               </span>
               <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-                I've built multiple web applications that demonstrate my skills
-                in creating responsive and interactive user interfaces scuh as a{" "}
+                I&apos;ve built multiple web applications that demonstrate my skills
+                in creating responsive and interactive user interfaces such as a{" "}
                 <Link
                   href="https://www.botify.freewebhostmost.com/"
-                  className="text-transparent bg-clip-text bg-gradient-to-r from-green-500 via-green-300 to-lime-500 font-semibold hover:underline">
+                  className="text-transparent bg-clip-text bg-gradient-to-r from-green-500 via-green-300 to-lime-500 font-semibold hover:underline"
+                >
                   Spotify Clone
-                </Link> and <Link
+                </Link>{" "}
+                and{" "}
+                <Link
                   href="https://www.botify.freewebhostmost.com/"
-                  className="text-transparent bg-clip-text bg-gradient-to-r from-green-500 via-green-300 to-lime-500 font-semibold hover:underline">
+                  className="text-transparent bg-clip-text bg-gradient-to-r from-green-500 via-green-300 to-lime-500 font-semibold hover:underline"
+                >
                   Todo list using react
-                </Link> and some others
+                </Link>{" "}
+                and some others.
               </p>
             </li>
           </ul>
