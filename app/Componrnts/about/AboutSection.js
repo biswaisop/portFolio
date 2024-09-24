42e60eaeaafd4830ede87dd5e7aeb576044ed3ba
Biswadip mandal <95963808+biswaisop@users.noreply.github.com>
2024-09-24 16:47:33 +0530

    aboutleft

--- app/Componrnts/about/AboutSection.js
+++ app/Componrnts/about/AboutSection.js
@@ -6,33 +6,29 @@ const AboutSection = () => {
   return (
     <section className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <div>
-          <h1 className="text-4xl font-bold mb-6">About Me</h1>
-          <p className="mt-4">
-            Hello! I'm Biswadip Mandal, a first-semester Computer Science
-            Engineering student at RVITM. I'm passionate about web development
-            and currently honing my skills with the MERN stack. I've already
-            built several projects that showcase my ability to create responsive
-            and dynamic web applications.
+      <div className="ml-4 lg:ml-8 mt-6 sm:mt-0">
+          <Image
+            src={aboutImage}
+            width={400}
+            height={400}
+            className="rounded-lg"
+            alt="About Image"
+          />
+        </div>
+        <div className="text-center md:text-left ml-4 lg:ml-8">
+          <h1 className="font-extrabold text-4xl lg:text-6xl mb-4">About Me</h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Hello! I'm Biswadip Mandal, a first-semester Computer Science Engineering student at RVITM. I'm passionate about web development and currently honing my skills with the MERN stack. I've already built several projects that showcase my ability to create responsive and dynamic web applications.
           </p>
-          <p className="mt-4">
-            In addition to web development, I have a strong foundation in Python,
-            having created various projects that demonstrate my problem-solving
-            skills. I'm also familiar with SQL, which helps me manage and
-            interact with databases efficiently.
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            In addition to web development, I have a strong foundation in Python, having created various projects that demonstrate my problem-solving skills. I'm also familiar with SQL, which helps me manage and interact with databases efficiently.
           </p>
-          <p className="mt-4">
-            As I continue my journey in tech, I'm excited to learn, grow, and take on
-            new challenges!
+          <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl">
+            As I continue my journey in tech, I'm excited to learn, grow, and take on new challenges!
           </p>
         </div>
-        <Image
-          src={aboutImage}
-          width={300}
-          height={300}
-          className="rounded-lg"
-          alt="About Image"
-        />
+        
+        
       </div>
     </section>
   );
