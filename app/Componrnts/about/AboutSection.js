7b7597c80d9c599cbc5ba2ac6ae1ea575566fe6e
Biswadip mandal <95963808+biswaisop@users.noreply.github.com>
2024-09-24 16:29:07 +0530

    aboutfix

--- app/Componrnts/about/AboutSection.js
+++ app/Componrnts/about/AboutSection.js
@@ -4,21 +4,25 @@ import aboutImage from "./images/used.png";
 
 const AboutSection = () => {
   return (
-    <section className="text-white ">
+    <section className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        
         <div>
-          <h1 className="font-xl">About Me</h1>
-          <p>
+          <h1 className="text-4xl font-bold mb-6">About Me</h1>
+          <p className="mt-4">
             Hello! I'm Biswadip Mandal, a first-semester Computer Science
             Engineering student at RVITM. I'm passionate about web development
             and currently honing my skills with the MERN stack. I've already
             built several projects that showcase my ability to create responsive
-            and dynamic web applications.</p> <p>In addition to web development, I have
-            a strong foundation in Python, having created various projects that
-            demonstrate my problem-solving skills. I'm also familiar with SQL,
-            which helps me manage and interact with databases efficiently.</p> <p> As I
-            continue my journey in tech, I'm excited to learn, grow, and take on
+            and dynamic web applications.
+          </p>
+          <p className="mt-4">
+            In addition to web development, I have a strong foundation in Python,
+            having created various projects that demonstrate my problem-solving
+            skills. I'm also familiar with SQL, which helps me manage and
+            interact with databases efficiently.
+          </p>
+          <p className="mt-4">
+            As I continue my journey in tech, I'm excited to learn, grow, and take on
             new challenges!
           </p>
         </div>
