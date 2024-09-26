17bbd37d2d04903fdd9242f710bdeb02988e118c
Biswadip mandal <95963808+biswaisop@users.noreply.github.com>
2024-09-26 14:53:29 +0530

    about

--- app/Componrnts/about/AboutSection.js
+++ app/Componrnts/about/AboutSection.js
@@ -7,7 +7,7 @@ import Link from "next/link";
 const AboutSection = () => {
   return (
     <section className="text-white">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="md:grid md:grid-cols-20 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <div className="ml-4 lg:ml-8 mt-3 sm:mt-0 mb-8 sm:mb-0">
           {" "}
           <h1 className="font-extrabold text-4xl lg:text-6xl mb-4">
@@ -35,7 +35,7 @@ const AboutSection = () => {
             </span>
             <p className="text-[#ADB7BE] text-base sm:text-lg mt-2 mb-6 lg:text-xl ">
               Web development using the MERN stack (MongoDB, Express, React,
-              Node.js).
+              Node.js) and other frameworks.
             </p>
           </div>
         </div>
